9927ac446c6fe8049789daede9485b686853fa83
Felix Delattre <felix@delattre.de>
2020-08-09 10:15:22 +0000

    provided certs to coturn server

--- build/coturn/files/start.sh
+++ build/coturn/files/start.sh
@@ -16,12 +16,12 @@ echo "listening-ip=$TURN_EXTERNAL_IP" >> /etc/turnserver.conf
 mkdir -p /tmp/certs && mkdir -p /etc/coturn/certs
 if [ -f /cert/acme.json ]; then
     traefik-certs-dumper file --source /cert/acme.json --dest /tmp/certs --version v2
-    mv /tmp/certs/certs/${TURN_REALM}.crt /tmp/certs/private/${TURN_REALM}.key /etc/coturn/certs/
+    mv /tmp/certs/certs/${TURN_HOST}.crt /tmp/certs/private/${TURN_HOST}.key /etc/coturn/certs/
 fi
 
 # paths to conntecion certificates for encryption
-echo "cert=/etc/prosody/certs/$TURN_REALM.key" #cert.pem
-echo "pkey=/etc/prosody/certs/$TURN_REALM.crt" #privkey.pem
+echo "cert=/etc/prosody/certs/$TURN_HOST.key" #cert.pem
+echo "pkey=/etc/prosody/certs/$TURN_HOST.crt" #privkey.pem
 
 # main turn server ports
 echo "listening-port=3478" >> /etc/turnserver.conf
@@ -35,8 +35,6 @@ echo "max-port=65535" >> /etc/turnserver.conf
 echo "fingerprint" >> /etc/turnserver.conf
 # send output to stdout for docker setup
 echo "log-file stdout" >> /etc/turnserver.conf
-#dissalow connections on lo interface
-echo "no-loopback-peers" >> /etc/turnserver.conf
 # disable command-line access 
 echo "no-cli" >> /etc/turnserver.conf
 
