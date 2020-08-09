0c07c544d8863d275d87ca461eed3e42ed296ad7
Felix Delattre <felix@delattre.de>
2020-08-09 10:36:05 +0000

    fixed keys for coturn server

--- build/coturn/files/start.sh
+++ build/coturn/files/start.sh
@@ -10,18 +10,19 @@ echo "static-auth-secret=$SECRET" >> /etc/turnserver.conf
 
 # your external IP, needed for some connections
 echo "external-ip=$TURN_EXTERNAL_IP" >> /etc/turnserver.conf
-echo "listening-ip=$TURN_EXTERNAL_IP" >> /etc/turnserver.conf
 
 # Prepare certificates to be in the default location where coturn expects them
 mkdir -p /tmp/certs && mkdir -p /etc/coturn/certs
 if [ -f /cert/acme.json ]; then
-    traefik-certs-dumper file --source /cert/acme.json --dest /tmp/certs --version v2
-    mv /tmp/certs/certs/${TURN_HOST}.crt /tmp/certs/private/${TURN_HOST}.key /etc/coturn/certs/
+    traefik-certs-dumper file --crt-ext=.pem --key-ext=.pem --source /cert/acme.json --dest /tmp/certs --version v2
+    if [ -f /tmp/certs/certs/${TURN_HOST}/privatekey.pem ]; then
+        mv tmp/certs/certs/${TURN_HOST}/certificate.pem /tmp/certs/certs/${TURN_HOST}/privatekey.pem /etc/coturn/certs/
+        # paths to conntecion certificates for encryption
+        echo "cert=/etc/coturn/certs/certificate.pem"
+        echo "pkey=/etc/coturn/certs/privatekey.pem"
+    fi
 fi
 
-# paths to conntecion certificates for encryption
-echo "cert=/etc/prosody/certs/$TURN_HOST.key" #cert.pem
-echo "pkey=/etc/prosody/certs/$TURN_HOST.crt" #privkey.pem
 
 # main turn server ports
 echo "listening-port=3478" >> /etc/turnserver.conf
