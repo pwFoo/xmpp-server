7174d0080e6d2090ef780cb3301cc26cca04843a
Felix Delattre <felix@delattre.de>
2020-08-09 13:24:46 +0000

    replaced coturn with eturnal

---
+++ build/eturnal/files/start.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Check if required variables are available
+if [ -z "${XMPP_SERVER_URL}" ]; then
+    echo "Failure starting xmpp server: The environment variable XMPP_SERVER_URL must be set."
+elif [ -z "${XMPP_GROUPS_URL}" ]; then
+    echo "Failure starting xmpp server: The environment variable XMPP_GROUPS_URL must be set."
+elif [ -z "${ADMIN_EMAIL}" ]; then
+    echo "Failure starting xmpp server: The environment variable ADMIN_EMAIL must be set."
+elif [ -z "${ADMIN_XMPP}" ]; then
+    echo "Failure starting xmpp server: The environment variable ADMIN_XMPP must be set."
+elif [ -z "${SECRET}" ]; then
+    echo "Failure starting xmpp server: The environment variable SECRET must be set."
+elif [ -z "${TURN_HOST}" ]; then
+    echo "Failure starting xmpp server: The environment variable TURN_HOST must be set."
+else
+
+    echo "Welcome to your turn-server"
+    echo "Main server url: ${TURN_HOST}"
+    echo "IPv4 address: ${TURN_IPV4}"
+    echo "IPv6 address: ${TURN_IPV6}"
+
+    # Replace required variables with configuration
+    sed -i "s#{{SECRET}}#${SECRET}#" /etc/eturnal.yml
+    sed -i "s#{{TURN_IPV4}}#${TURN_IPV4}#" /etc/eturnal.yml
+    sed -i "s#{{TURN_IPV6}}#${TURN_IPV6}#" /etc/eturnal.yml
+
+    # Prepare certificates to be in the default location where eturnal expects them
+    mkdir -p /tmp/certs && mkdir -p /etc/eturnal/tls/
+    if [ -f /cert/acme.json ]; then
+        traefik-certs-dumper file --domain-subdir --crt-ext=.pem --key-ext=.pem --source /cert/acme.json --dest /tmp/certs --version v2
+        if [ -f /tmp/certs/${TURN_HOST}/privatekey.pem ]; then
+            mv /tmp/certs/${TURN_HOST}/certificate.pem /tmp/certs/${TURN_HOST}/privatekey.pem /etc/eturnal/tls/
+        else
+            echo "ERROR: Traefik generated letsencrypt certificates not found!"
+        fi
+    fi
+
+exec eturnalctl foreground "$@"
